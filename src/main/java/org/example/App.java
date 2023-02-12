31aa475fcdae8ee6dc5447fc443f669d1c72917b
VeraSlaveholder <bulgakova.vera2003@mail.ru>
2023-02-12 03:29:59 +0300

    cascading in Hibernate(54)

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,6 +1,8 @@
 package org.example;
 
+import org.example.model.Actor;
 import org.example.model.Item;
+import org.example.model.Movie;
 import org.example.model.Person;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -8,32 +10,24 @@ import org.hibernate.cfg.Configuration;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 
 public class App {
     public static void main(String[] args) {
         Configuration configuration = new Configuration().addAnnotatedClass(Person.class)
                 .addAnnotatedClass(Item.class);
-
         SessionFactory sessionFactory = configuration.buildSessionFactory();
-        Session session = sessionFactory.getCurrentSession();
-        try {
+
+        try (sessionFactory) {
+            Session session = sessionFactory.getCurrentSession();
             session.beginTransaction();
+            Person person = new Person("Test cascading", 30);
 
-            Person person=session.get(Person.class,4);
-            Item item=session.get(Item.class,1);
-            item.getOwner().getItems().remove(item);
+            person.addItem(new Item("Item1"));
+            person.addItem(new Item("Item2"));
+            person.addItem(new Item("Item3"));
 
-            item.setOwner(person);
-            person.getItems().add(item);
-
+            session.save(person);
             session.getTransaction().commit();
-
-        } finally {
-
-            sessionFactory.close();
         }
-
-
     }
 }
--- src/main/java/org/example/model/Person.java
+++ src/main/java/org/example/model/Person.java
@@ -1,6 +1,9 @@
 package org.example.model;
 
+import org.hibernate.annotations.Cascade;
+
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -15,6 +18,7 @@ public class Person {
     @Column(name = "age")
     private int age;
     @OneToMany(mappedBy = "owner")
+    @Cascade(org.hibernate.annotations.CascadeType.SAVE_UPDATE)
     private List<Item> items;
 
     public List<Item> getItems() {
@@ -64,5 +68,11 @@ public class Person {
                 ", name='" + name + '\'' +
                 ", age=" + age +
                 '}';
+    }
+    public void addItem(Item item){
+        if(this.items==null)
+            this.items=new ArrayList<>();
+        this.items.add(item);
+        item.setOwner(this);
     }
 }
